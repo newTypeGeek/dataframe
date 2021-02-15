d403e3497cf2a89842d9c6150142e2220883bd74
bpht <bennyphtam@gmail.com>
2021-02-15 18:43:44 +0800

    - Declared Series constructors, display methods and getter methods - Implemented Series constructors only

--- src/series/series.cpp
+++ src/series/series.cpp
@@ -1,17 +1,38 @@
 #include <string>
 #include <vector>
+#include <cstdio>
 #include "series.hpp"
 
-Series::Series(std::vector<double> data, std::vector<std::string> index, std::string name) {
+
+template <typename Dtype>
+Series::Series(const std::vector<Dtype>& data) {
     data_ = data;
-    index_ = index;
+
+    // If no index argument is provided, use default index setting as shown below
+    for (int i = 0; i < data_.size(); ++i) {
+        index_.push_back(i);
+    }
+
+    // default name is empty string
+    name_ = "";
+}
+
+template <typename Dtype>
+Series::Series(const std::vector<Dtype>& data, std::string name) {
+    data_ = data;
+
+    // If no index argument is provided, use default index setting as shown below
+    for (int i = 0; i < data_.size(); ++i) {
+        index_.push_back(i);
+    }
+
     name_ = name;
 }
 
-double Series::sum() {
-    double total = 0.;
-    for (double value : data_) {
-        total += value;
-    }
-    return total;
+
+template <typename Dtype>
+Series::Series(const std::vector<Dtype>& data, const std::vector<Dtype>& index, std::string name) {
+    data_ = data;
+    index_ = index;
+    name_ = name;
 }
--- src/series/series.hpp
+++ src/series/series.hpp
@@ -2,18 +2,35 @@
 #define SERIES_HPP
 #include <string>
 #include <vector>
+#include <iostream>
 
+template <class Dtype>
 class Series {
     private:
       // TODO: create template class for data and index to accept dynamic type
-      std::vector<double> data_;
-      std::vector<std::string> index_;
+      std::vector<Dtype> data_;
+      std::vector<Dtype> index_;
       std::string name_;
     
     public:
-      Series(std::vector<double> data, std::vector<std::string> index, std::string name);
+      Series(const std::vector<Dtype>& data);
+      Series(const std::vector<Dtype>& data, std::string name);
+      Series(const std::vector<Dtype>& data, const std::vector<Dtype>& index, std::string name);
 
-      // temporary method for testing
+      // head displays Series with first num_rows rows
+      void head();
+      void head(int: num_rows);
+
+      // tail displays Series with last num_rows rows
+      void tail();
+      void tail(int: num_rows);
+
+      // Getter
+      std::vector<Dtype> values();
+      std::vector<Dtype> index();
+      std::string name();
+
+      // sum over all values in Series if Dtype is numerical
       double sum();
 };
 #endif
