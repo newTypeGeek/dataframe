816dca4524f0d63119bfd5f0745c46eb3a3b62b1
bpht <bennyphtam@gmail.com>
2021-02-16 21:58:19 +0800

    Deleted head, tail and sum declaration

--- src/series/series.hpp
+++ src/series/series.hpp
@@ -18,20 +18,10 @@ class Series {
       Series(const std::vector<Dtype>& data, std::string name);
       Series(const std::vector<Dtype>& data);
 
-      // head displays Series with first num_rows rows
-      void head();
-      void head(int num_rows);
-
-      // tail displays Series with last num_rows rows
-      void tail();
-      void tail(int num_rows);
-
       // Getter
       std::vector<Dtype> values();
       std::vector<Dtype> index();
       std::string name();
 
-      // sum over all values in Series if Dtype is numerical
-      double sum();
 };
 #endif
